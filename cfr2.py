ecfab0c5653cb1bf233600798d2cc32dec903a27
Andrew MacGinitie <macginitie@charter.net>
2020-06-10 14:48:35 -0400

    formatting closer to PEP-8

--- cfr2.py
+++ cfr2.py
@@ -11,17 +11,20 @@ df = pd.read_csv(csvfile)
 # hacks to deal with missing values
 last_recorded = 0
 
+
 def diff(data, index):
     global last_recorded
     if index < 2:
         return 0
+        
     subtrahend = 0
-    minuend = 0
     try:
         subtrahend = int(data[index - 1])
         last_recorded = subtrahend
     except:
         print('! @{0}=>{1} - {2}'.format(index, data[index], data[index - 1]))
+        
+    minuend = 0
     try:
         minuend = int(data[index])
         last_recorded = minuend
@@ -30,9 +33,12 @@ def diff(data, index):
 
     if minuend < subtrahend or minuend == 0:
         return 0
+        
     if subtrahend == 0:
         return minuend - last_recorded
+        
     return minuend - subtrahend
+    
         
 def smooth(data):
     for idx in range(len(data)- 1):
@@ -40,6 +46,7 @@ def smooth(data):
             # 2DO: make this work for multi-day skips too
             data[idx] = data[idx+1]/2
             data[idx+1] /= 2
+            
 
 diff_world = []
 wrld_data = df[' worldwide deaths per Wikipedia']
@@ -48,12 +55,14 @@ for idx in range(len(wrld_data)):
 
 smooth(diff_world)
 
+
 diff_US = []
 us_data = df[' U.S. deaths per Wikipedia']
 for idx in range(len(us_data)):
     diff_US.append(diff(us_data, idx))
     
 smooth(diff_US)    
+
 
 fig = make_subplots(
     rows=1, cols=2,
