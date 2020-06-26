aa995e149fbd773b97cf2a8e6dadaef72342912d
Andrew MacGinitie <macginitie@charter.net>
2020-06-26 12:08:03 -0400

    brevity is the soul of something

--- inmiwb.py
+++ inmiwb.py
@@ -6,8 +6,7 @@ now = dt.now()
 #print(now.date())
 
 """
-inmfniwb = 
-In N Minutes From Now It Will Be
+inmiwb = In N Minutes It Will Be
 """
 
 
@@ -15,7 +14,7 @@ if __name__ == '__main__':
     try:   
         mfn = int(sys.argv[1])
     except:
-        print('please provide a # of minutes as arg #1')
+        print('please provide # of minutes as arg #1')
         exit(0)
         
     print('in {0} minutes it will be {1}'.format(mfn, (now + td(minutes=mfn))))
